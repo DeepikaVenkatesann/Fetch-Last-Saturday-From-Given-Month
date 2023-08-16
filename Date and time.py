7daf857e74505bc636e71eaab3b36ca4deebdecf
DeepikaVenkatesann <141828515+DeepikaVenkatesann@users.noreply.github.com>
2023-08-16 16:43:45 +0530

    Update Date and time.py

--- Date and time.py
+++ Date and time.py
@@ -1,13 +1,13 @@
 def lastsaturday(month, year):
-    last_day = datetime(year, month + 1, 1) - timedelta(days=1)
-    weekdays = last_day.isoweekday()
-    last_sat = last_day - timedelta(days=weekdays + 1)
+    lastday = datetime(year, month + 1, 1) - timedelta(days=1)
+    weekdays = lastday.isoweekday()
+    last_sat = lastday - timedelta(days=weekdays + 1)
     return last_sat.day, (last_day.day - last_sat.day) // 7 + 1
 
 def totalsaturdays(month, year):
     firstday = datetime(year, month, 1)
-    numsaturdays = (firstday.weekday() - 7) % 7
-    return (firstday + timedelta(days=numsaturdays)).day
+    numofsaturdays = (firstday.weekday() - 7) % 7
+    return (firstday + timedelta(days=numofsaturdays)).day
 
 input_month_year = "062023"
 month = int(input_month_year[:2])
