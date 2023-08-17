032b7c25bcbcfd17868fc3cdf6194f25982e3475
DeepikaVenkatesann <141828515+DeepikaVenkatesann@users.noreply.github.com>
2023-08-17 10:51:47 +0530

    Update Date and time.py

--- Date and time.py
+++ Date and time.py
@@ -1,20 +1,19 @@
+from datetime import datetime, timedelta
 def lastsaturday(month, year):
     lastday = datetime(year, month + 1, 1) - timedelta(days=1)
     weekdays = lastday.isoweekday()
-    lastsaturday = lastday - timedelta(days=weekdays + 1)
-    return lastsaturday.day, (lastday.day - lastsaturday.day) // 7 + 1
+    days_to_subtract = (weekdays - 5) % 7 
+    lastsaturday = lastday - timedelta(days=days_to_subtract)
+    return lastsaturday.day
 
 def totalsaturdays(month, year):
-    firstday = datetime(year, month, 1)
-    numofsaturdays = (firstday.weekday() - 7) % 7
-    return (firstday + timedelta(days=numofsaturdays)).day
-
-input_month_year = "062023"
-month = int(input_month_year[:2])
-year = int(input_month_year[2:])
-
-last_sat, total_sats = lastsaturday(month, year)
-total_sats = totalsaturdays(month, year)
+    first_day = datetime(year, month, 1)
+    last_day = datetime(year, month + 1, 1) - timedelta(days=1)
+    num_saturdays = sum(1 for day in range((last_day - first_day).days + 1) if (first_day + timedelta(days=day)).weekday() == 5)
+    return num_saturdays
+    
+last_sat, total_sats = lastsaturday(month=6, year=2023)
+total_sats = totalsaturdays(month=6, year=2023)
 
 print("Last Saturday:",last_sat)
 print("Total Saturdays:",total_sats)
